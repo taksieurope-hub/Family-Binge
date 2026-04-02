2388217f1888189a1433d4608247b42f5101f395
Max <max@example.com>
2026-04-02 13:23:55 +0400

    fix iframe sandbox - allow popups for video players

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -35,8 +35,10 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     fetch(event.request)
       .then(response => {
-        const clone = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        if (event.request.method === "GET") {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        }
         return response;
       })
       .catch(() => caches.match(event.request))
