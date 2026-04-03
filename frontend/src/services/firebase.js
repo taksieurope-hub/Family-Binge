310785ca3087a68ec5f29f27b5687fd33b381567
Max <max@example.com>
2026-04-03 12:11:36 +0400

    update Firebase config to new project

--- frontend/src/services/firebase.js
+++ frontend/src/services/firebase.js
@@ -1,19 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDJBimKwUKSoANAIoy5p9aKVr5z7SifJF0",
-  authDomain: "family-binge.firebaseapp.com",
-  projectId: "family-binge",
-  storageBucket: "family-binge.firebasestorage.app",
-  messagingSenderId: "270385704188",
-  appId: "1:270385704188:web:a21f6b35e0dd4c7ee13b79",
-  measurementId: "G-Q4J010D7ML"
+  apiKey: "AIzaSyBVc5NCQ8veu-AVQt3EA9DcH6KWEueEVv4",
+  authDomain: "family-binge-86da4.firebaseapp.com",
+  projectId: "family-binge-86da4",
+  storageBucket: "family-binge-86da4.firebasestorage.app",
+  messagingSenderId: "1067511376542",
+  appId: "1:1067511376542:web:bcbec7a4fb2b623727298f",
+  measurementId: "G-WGQVQB5VLJ"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const analytics = getAnalytics(app);
